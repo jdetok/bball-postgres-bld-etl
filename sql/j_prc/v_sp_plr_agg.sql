0f73e029f35930f4cdc6efc92b470f9477e9e102
jdetok <jdeko17@gmail.com>
2025-08-07 14:41:40 -0500

    do nothing on conflict

--- sql/j_prc/v_sp_plr_agg.sql
+++ sql/j_prc/v_sp_plr_agg.sql
@@ -18,6 +18,9 @@ drop view if exists api.v_plr_cc_avg;
 -- ============================================================================
 /* 
 STORED PROCEDURE TO INSERT THE RESULTS OF THE VIEWS ABOVE INTO API TABLE
+HAD TO ADD ON CONFLICT DO NOTHING TO EACH INSERT
+EDITED AND MOVED EACH VIEW TO ITS OWN FILE. FOR CAREER VIEWS, JOINED TO TEAM ON
+TEAM WITH MOST GAMES/MINUTES PLAYED
 */ 
 create or replace procedure api.sp_plr_agg()
 language plpgsql
@@ -28,28 +31,40 @@ begin
 
 	-- season aggs
 	raise notice 'inserting season totals';
-    insert into api.plr_agg select * from api.v_plr_szn_tot;
+    insert into api.plr_agg 
+		select * from api.v_plr_szn_tot
+	on conflict(player_id, season_id, stat_type) do nothing;
 	raise notice e'season totals complete: %\n', public.fn_cntstr('api.plr_agg');
 
 	raise notice 'inserting season avgs';
-	insert into api.plr_agg select * from api.v_plr_szn_avg;
+	insert into api.plr_agg 
+		select * from api.v_plr_szn_avg
+	on conflict(player_id, season_id, stat_type) do nothing;
 	raise notice e'season avgs complete: %\n', public.fn_cntstr('api.plr_agg');
 
 	-- reg season/playoff aggs
 	raise notice 'inserting rs/playoff totals';
-    insert into api.plr_agg select * from api.v_plr_rp_tot;
+    insert into api.plr_agg 
+		select * from api.v_plr_rp_tot
+	on conflict(player_id, season_id, stat_type) do nothing;
 	raise notice e'regszn/playoff totals complete: %\n', public.fn_cntstr('api.plr_agg');
 
 	raise notice 'inserting rs/playoff avgs';
-	insert into api.plr_agg select * from api.v_plr_rp_avg;
+	insert into api.plr_agg 
+		select * from api.v_plr_rp_avg
+	on conflict(player_id, season_id, stat_type) do nothing;
 	raise notice e'regszn/playoff avgs complete: %\n', public.fn_cntstr('api.plr_agg');
 
 	-- combined reg season/playoff aggs
 	raise notice 'inserting combined rs/playoff totals';
-    insert into api.plr_agg select * from api.v_plr_cc_tot;
+	insert into api.plr_agg
+		select * from api.v_plr_cc_tot
+	on conflict(player_id, season_id, stat_type) do nothing;
 	raise notice e'combined totals complete: %\n', public.fn_cntstr('api.plr_agg');
 
 	raise notice 'inserting combined rs/playoff avgs';
-	insert into api.plr_agg select * from api.v_plr_cc_avg;
+	insert into api.plr_agg 
+		select * from api.v_plr_cc_avg
+	on conflict(player_id, season_id, stat_type) do nothing;
 	raise notice e'combined avgs complete: %\n', public.fn_cntstr('api.plr_agg');
 end; $$;
