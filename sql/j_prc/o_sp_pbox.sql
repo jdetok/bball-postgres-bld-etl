618b0e855a01404330e824db529576e3afa30ba7
jdetok <jdeko17@gmail.com>
2025-08-07 14:27:33 -0500

    views get their own file

--- sql/j_prc/o_sp_pbox.sql
+++ sql/j_prc/o_sp_pbox.sql
@@ -38,3 +38,40 @@ begin
 end; $$;
 -- call stats.sp_pbox();
 -- select * from stats.pbox;
+create or replace procedure stats.sp_pbox()
+language plpgsql
+as $$
+begin
+    insert into stats.pbox
+        select
+            a.season_id,
+            a.team_id, 
+            a.game_id,
+            a.player_id,
+            a.game_date,
+            a.matchup,
+            a.wl,
+            a.min,
+            a.pts,
+            a.ast,
+            a.reb,
+            a.stl,
+            a.blk,
+            a.tov,
+            a.oreb,
+            a.dreb,
+            a.pf,
+            a.plus_minus,
+            a.fgm, 
+            a.fga,
+            a.fg_pct,
+            a.fg3m,
+            a.fg3a,
+            a.fg3_pct,
+            a.ftm,
+            a.fta,
+            a.ft_pct
+        from intake.gm_player a 
+        inner join lg.plr b on b.player_id = a.player_id
+    on conflict (team_id, game_id, player_id) do nothing;
+end; $$;
